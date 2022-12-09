11f382f7f1e783acd964ded8890bf8b9a15819bd
Murilo Muller <murilomuller@protonmail.com>
2022-12-09 20:59:43 -0300

    Projeto inicial finalizado

--- 01-FundamentosNodeJS/src/index.js
+++ 01-FundamentosNodeJS/src/index.js
@@ -1,4 +1,3 @@
-const { json } = require('express')
 const express = require('express')
 const { v4: uuidv4 } = require('uuid')
 
@@ -10,8 +9,8 @@ const { v4: uuidv4 } = require('uuid')
  */
 
 const app = express()
-
 app.use(express.json())
+const customers = []
 
 function verifyIfExistsAccountCPF(request, response, next) {
   const { cpf } = request.headers
@@ -20,10 +19,20 @@ function verifyIfExistsAccountCPF(request, response, next) {
   if (!customer) {
     return response.status(400).json({ error: 'Customer not found!' })
   }
+  request.customer = customer
   return next()
 }
 
-const customers = []
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if (operation.type === 'credit') {
+      return acc + operation.amount
+    } else {
+      return acc - operation.amount
+    }
+  }, 0)
+  return balance
+}
 
 app.post('/account', (request, response) => {
   const { cpf, name } = request.body
@@ -37,9 +46,9 @@ app.post('/account', (request, response) => {
   }
 
   customers.push({
-    id: uuidv4(),
     cpf,
     name,
+    id: uuidv4(),
     statement: [],
   })
   return response.status(201).send()
@@ -48,8 +57,90 @@ app.post('/account', (request, response) => {
 // app.use(verifyIfExistsAccountCPF)
 // Todas requisições abaixo desse middleware terão que passar por estas verificações
 
-app.get('/statement/', verifyIfExistsAccountCPF, (request, response) => {
+app.get('/statement', verifyIfExistsAccountCPF, (request, response) => {
   const { customer } = request
   return response.status(200).json(customer.statement)
 })
-app.listen(3333)
+
+app.post('/deposit', verifyIfExistsAccountCPF, (request, response) => {
+  const { description, amount } = request.body
+
+  const { customer } = request
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: 'credit',
+  }
+
+  customer.statement.push(statementOperation)
+  return response.status(201).send()
+})
+
+app.post('/withdraw', verifyIfExistsAccountCPF, (request, response) => {
+  const { amount } = request.body
+  const { customer } = request
+
+  const balance = getBalance(customer.statement)
+  if (balance < amount) {
+    return response.status(400).json({ error: 'Insufficient funds!' })
+  }
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: 'debit',
+  }
+
+  customer.statement.push(statementOperation)
+
+  return response.status(201).send()
+})
+
+app.get('/statement/date', verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request
+  const { date } = request.query
+
+  const dateFormat = new Date(date + ' 00:00')
+
+  const statement = customer.statement.filter(
+    (statement) =>
+      statement.created_at.toDateString() ===
+      new Date(dateFormat).toDateString(),
+  )
+
+  return response.json(statement)
+})
+
+app.put('/account', verifyIfExistsAccountCPF, (request, response) => {
+  const { name } = request.body
+  const { customer } = request
+
+  customer.name = name
+
+  return response.status(201).send()
+})
+
+app.get('/account', verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request
+  return response.json(customer)
+})
+
+app.delete('/account', verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request
+
+  customers.splice(customer, 1)
+
+  return response.status(200).json(customers)
+})
+
+app.get('/balance', verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request
+  const balance = getBalance(customer.statement)
+
+  return response.json(balance)
+})
+
+app.listen(3333, () => {
+  console.log('server está rodando na porta 3333')
+})
