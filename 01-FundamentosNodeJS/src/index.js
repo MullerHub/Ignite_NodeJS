a9ad578bc2fea633d3ab1412c5c1ac50399e12ff
Murilo Muller <murilomuller@protonmail.com>
2022-12-09 12:23:23 -0300

    Cadastro de conta

--- 01-FundamentosNodeJS/src/index.js
+++ 01-FundamentosNodeJS/src/index.js
@@ -1,9 +1,31 @@
+const { json } = require('express')
 const express = require('express')
+const { v4: uuidv4 } = require('uuid')
 
 const app = express()
 
-app.get('/courses', (request, response) => {
-  return response.json({ message: 'Hello world!' })
+app.use(express.json())
+
+/**
+ * cpf- string
+ * name- string
+ * id- uuid
+ * statement []
+ */
+
+const customers = []
+
+app.post('/account', (request, response) => {
+  const id = uuidv4()
+  const { cpf, name } = request.body
+
+  customers.push({
+    id,
+    cpf,
+    name,
+    statement: [],
+  })
+  return response.status(201).send()
 })
 
 app.listen(3333)
