e2f905a15aba66ec2894679648e80c5136f94793
Murilo Muller <murilomuller@protonmail.com>
2022-12-13 18:58:16 -0300

    Trabalhando com banco de dados init

--- 03-iniciando-a-api/src/modules/cars/useCases/importCategories/ImportCategoriesUseCase.ts
+++ 03-iniciando-a-api/src/modules/cars/useCases/importCategories/ImportCategoriesUseCase.ts
@@ -1,57 +1,60 @@
-import fs from 'fs';
-import csvParse from 'csv-parse';
-import { CategoriesRepository } from '../../repositories/implementations/CategoriesRepository';
+import fs from 'fs'
+import csvParse from 'csv-parse'
+import { CategoriesRepository } from '../../repositories/implementations/CategoriesRepository'
 
 interface IImportCategory {
-  name: string,
-  description: string,
+  name: string
+  description: string
 }
 
 class ImportCategoriesUseCase {
-  private categoriesRepository: CategoriesRepository;
+  private categoriesRepository: CategoriesRepository
   constructor(categoriesRepository: CategoriesRepository) {
-    this.categoriesRepository = categoriesRepository;
-  };
+    this.categoriesRepository = categoriesRepository
+  }
 
   loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
     return new Promise((resolve, reject) => {
-      const stream = fs.createReadStream(file.path);
-      const categories: IImportCategory[] = [];
+      const stream = fs.createReadStream(file.path)
+      const categories: IImportCategory[] = []
 
-      const parseFile = csvParse();
+      const parseFile = csvParse()
 
-      stream.pipe(parseFile);
+      stream.pipe(parseFile)
 
-      parseFile.on("data", async (line) => {
-        const [ name, description ] = line;
-        categories.push({
-          name,
-          description
-        });
-      }).on("end", () => {
-        fs.promises.unlink(file.path);
-        resolve(categories);
-      }).on("error", (err) => {
-        reject(err);
-      });
-    });
-  };
+      parseFile
+        .on('data', async (line) => {
+          const [name, description] = line
+          categories.push({
+            name,
+            description,
+          })
+        })
+        .on('end', () => {
+          fs.promises.unlink(file.path)
+          resolve(categories)
+        })
+        .on('error', (err) => {
+          reject(err)
+        })
+    })
+  }
 
   async execute(file: Express.Multer.File): Promise<void> {
-    const categories = await this.loadCategories(file);
-    categories.map(async category => {
-      const { name, description } = category;
+    const categories = await this.loadCategories(file)
+    categories.map(async (category) => {
+      const { name, description } = category
 
-      const existCategory = this.categoriesRepository.findByName(name);
+      const existCategory = this.categoriesRepository.findByName(name)
 
-      if(!existCategory) {
+      if (!existCategory) {
         this.categoriesRepository.create({
           name,
-          description
-        });
-      };
-    });
+          description,
+        })
+      }
+    })
   }
 }
 
-export { ImportCategoriesUseCase };
+export { ImportCategoriesUseCase }
--- 03-iniciando-a-api/src/server.ts
+++ 03-iniciando-a-api/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import swaggerUi from 'swagger-ui-express'
+import './database'
 
 import swaggerFile from './swagger.json'
 
@@ -13,6 +14,6 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use(router)
 
-app.listen(3334, () => {
+app.listen(3333, () => {
   console.log('Server is up! :D')
 })
