0327e20624caee644cd9c32363dded88f3128c24
Murilo Muller <murilomuller@protonmail.com>
2022-12-17 00:12:45 -0300

    Tratamento de erros/ excessoes

--- 03-iniciando-a-api/src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ 03-iniciando-a-api/src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -1,6 +1,7 @@
 import { compare } from 'bcrypt'
 import { sign } from 'jsonwebtoken'
 import { inject, injectable } from 'tsyringe'
+import { AppError } from '../../../../errors/AppError'
 import { IUsersRepository } from '../../repositories/IUsersRepository'
 
 interface IRequest {
@@ -28,20 +29,24 @@ class AuthenticateUserUseCase {
     const user = await this.usersRepository.findByEmail(email)
 
     if (!user) {
-      throw new Error('Usuário ou senha não encontrados')
+      throw new AppError('Usuário ou senha não encontrados')
     }
 
     const passwordMatch = await compare(password, user.password)
     // Senha está correta
     if (!passwordMatch) {
-      throw new Error('Senha incorreta')
+      throw new AppError('Senha incorreta')
     }
 
     // Gerar jsonwebtoken
-    const token = sign({}, 'NecessitaCriptografiaNesseExemploUsadoNaAuthenticateEnoMiddleware', {
-      subject: user.id,
-      expiresIn: '1d',
-    })
+    const token = sign(
+      {},
+      'NecessitaCriptografiaNesseExemploUsadoNaAuthenticateEnoMiddleware',
+      {
+        subject: user.id,
+        expiresIn: '1d',
+      },
+    )
 
     const tokenReturn: IResponse = {
       token,
