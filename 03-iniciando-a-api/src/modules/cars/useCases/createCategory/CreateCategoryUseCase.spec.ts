8c261b67977747493f6d64492c80dbe3fcbf3826
Murilo Muller <murilomuller@protonmail.com>
2022-12-19 14:44:13 -0300

    Iniciando testes unitarios

---
+++ 03-iniciando-a-api/src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,12 @@
+describe('criar categoria', () => {
+  it('Espero que 2 + 2 seja 4', () => {
+    const soma = 2 + 2
+    const resultadoo = 4
+    expect(soma).toBe(resultadoo)
+  })
+  it('Espero que 2 + 2 não seja 5', () => {
+    const soma = 2 + 2
+    const resultadoo = 5
+    expect(soma).not.toBe(resultadoo)
+  })
+})
