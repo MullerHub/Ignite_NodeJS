631f3a67f56ef15136f53e46f67b5d0b33967369
Murilo Muller <murilomuller@protonmail.com>
2022-12-20 11:51:20 -0300

    Teste de autenticação do usuario

--- 03-iniciando-a-api/src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
+++ 03-iniciando-a-api/src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -1,12 +1,50 @@
-describe('criar categoria', () => {
-  it('Espero que 2 + 2 seja 4', () => {
-    const soma = 2 + 2
-    const resultadoo = 4
-    expect(soma).toBe(resultadoo)
+import { AppError } from '../../../../errors/AppError'
+import { CategoriesRepositoryInMemory } from '../../repositories/in-memory/CategoriesRepositoryInMemory'
+import { CreateCategoryUseCase } from './CreateCategoryUseCase'
+
+let createCategoryUseCase: CreateCategoryUseCase
+let categoriesRepositoryInMemory: CategoriesRepositoryInMemory
+
+describe('Create Category', () => {
+  beforeEach(() => {
+    categoriesRepositoryInMemory = new CategoriesRepositoryInMemory()
+    createCategoryUseCase = new CreateCategoryUseCase(
+      categoriesRepositoryInMemory,
+    )
   })
-  it('Espero que 2 + 2 não seja 5', () => {
-    const soma = 2 + 2
-    const resultadoo = 5
-    expect(soma).not.toBe(resultadoo)
+
+  it('should be able to create a new category', async () => {
+    const category = {
+      name: 'newCategory',
+      description: 'category description test',
+    }
+    await createCategoryUseCase.execute({
+      name: category.name,
+      description: category.description,
+    })
+    const categoryCreated = await categoriesRepositoryInMemory.findByName(
+      category.name,
+    )
+
+    console.log(categoryCreated)
+    expect(categoryCreated).toHaveProperty('id')
+  })
+
+  it('should be able to create a new category with same name', async () => {
+    expect(async () => {
+      const category = {
+        name: 'newCategory',
+        description: 'category description test',
+      }
+      await createCategoryUseCase.execute({
+        name: category.name,
+        description: category.description,
+      })
+
+      await createCategoryUseCase.execute({
+        name: category.name,
+        description: category.description,
+      })
+    }).rejects.toBeInstanceOf(AppError)
   })
 })
