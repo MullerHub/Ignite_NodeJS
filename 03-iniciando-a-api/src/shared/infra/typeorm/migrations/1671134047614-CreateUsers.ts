ef97da1128a2348305d8a31877edcd57c9447ba1
Murilo Muller <murilomuller@protonmail.com>
2022-12-30 16:41:34 -0300

    Criando migrations do aluguel

--- 03-iniciando-a-api/src/shared/infra/typeorm/migrations/1671134047614-CreateUsers.ts
+++ 03-iniciando-a-api/src/shared/infra/typeorm/migrations/1671134047614-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1671134047614 implements MigrationInterface {
           {
             name: 'id',
             type: 'uuid',
+            isPrimary: true,
           },
           {
             name: 'name',
