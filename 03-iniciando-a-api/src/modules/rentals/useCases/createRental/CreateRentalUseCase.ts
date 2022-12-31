1cfab99f5fb93d79d3aa45053c29edf74704eeaf
Murilo Muller <murilomuller@protonmail.com>
2022-12-31 02:30:18 -0300

    Criando controller / Aluguel finalizado

--- 03-iniciando-a-api/src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ 03-iniciando-a-api/src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -1,6 +1,8 @@
 import { Rental } from '@modules/rentals/infra/typeorm/entities/Rental'
 import { IRentalsRepository } from '@modules/rentals/repositories/IRentalsRepository'
+import { IDateProvider } from '@shared/container/providers/DateProvider/IDateProvider'
 import { AppError } from '@shared/errors/AppError'
+import { inject, injectable } from 'tsyringe'
 
 interface IRequest {
   user_id: string
@@ -8,25 +10,32 @@ interface IRequest {
   expected_return_date: Date
 }
 
+@injectable()
 class CreateRentalUseCase {
-  constructor(private rentalsRepository: IRentalsRepository) {}
+  constructor(
+    @inject('RentalsRepository')
+    private rentalsRepository: IRentalsRepository,
+    @inject('DayJsDateProvider')
+    private dateProvider: IDateProvider,
+  ) {}
 
   async execute({
     car_id,
     expected_return_date,
     user_id,
   }: IRequest): Promise<Rental> {
+    const minimumHour = 24
+
     // Não deve ser possivel cadastrar um novo aluguel caso já exista um aberto para o mesmo carro
     const carUnavailable = await this.rentalsRepository.findOpenRentalByCar(
       car_id,
     )
 
-    if (!carUnavailable) {
+    if (carUnavailable) {
       throw new AppError('Car is unavailable')
     }
 
     //Não deve ser possivel cadastrar um novo aluguel caso já exista um aberto para o mesmo usuario
-
     const rentalOpenToUser = await this.rentalsRepository.findOpenRentalByUser(
       user_id,
     )
@@ -36,7 +45,19 @@ class CreateRentalUseCase {
     }
 
     // O aluguel deve ter duração minima de 24 horas
+    const expectedReturnDateFormat =
+      this.dateProvider.convertToUTC(expected_return_date)
 
+    const dateNow = this.dateProvider.dateNow()
+
+    const compare = this.dateProvider.compareInHours(
+      dateNow,
+      expected_return_date,
+    )
+
+    if (compare < minimumHour) {
+      throw new AppError('Invalid return time!')
+    }
     const rental = await this.rentalsRepository.create({
       user_id,
       car_id,
--- 03-iniciando-a-api/src/shared/infra/http/routes/index.ts
+++ 03-iniciando-a-api/src/shared/infra/http/routes/index.ts
@@ -5,6 +5,7 @@ import { categoriesRoutes } from './categories.routes'
 import { specificationRoutes } from './specifications.routes'
 import { userRoutes } from './users.routes'
 import { carsRoutes } from './cars.routes'
+import { rentalRoutes } from './rental.routes'
 
 const router = Router()
 
@@ -12,6 +13,7 @@ router.use('/categories', categoriesRoutes)
 router.use('/specifications', specificationRoutes)
 router.use('/users', userRoutes)
 router.use('/cars', carsRoutes)
+router.use('/rentals', rentalRoutes)
 router.use(authenticateRoutes)
 
 export { router }
