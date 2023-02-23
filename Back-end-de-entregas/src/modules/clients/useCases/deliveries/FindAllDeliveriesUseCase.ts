1d3a532bb8234f4da95ce3a970bff2c90f50a7a6
Murilo Muller <murilomuller@protonmail.com>
2023-02-23 00:08:39 -0300

    Busca com select

--- Back-end-de-entregas/src/modules/clients/useCases/deliveries/FindAllDeliveriesUseCase.ts
+++ Back-end-de-entregas/src/modules/clients/useCases/deliveries/FindAllDeliveriesUseCase.ts
@@ -2,12 +2,14 @@ import { prisma } from '../../../../database/prismaClient'
 
 export class FindAllDeliveriesUseCase {
   async execute(id_client: string) {
-    const deliveries = await prisma.deliveries.findMany({
+    const deliveries = await prisma.clients.findMany({
       where: {
         id: id_client,
       },
-      include: {
+      select: {
         deliveries: true,
+        id: true,
+        username: true,
       },
     })
 
